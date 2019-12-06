5f82bcfd22e38117986694ae87fc35ef67b0e9f7
Calvin Tam <51683307+ctam4@users.noreply.github.com>
2019-12-05 16:18:24 -0800

    Add Asteroid, Moon

---
+++ src/galacticmail/gameobject/Asteroid.java
@@ -0,0 +1,62 @@
+package galacticmail.gameobject;
+
+import java.awt.image.BufferedImage;
+import java.lang.Math;
+
+import galacticmail.Launcher;
+
+public class Asteroid extends GameMovableObject {
+    private final int R = 3;
+
+    private int damage;
+
+    public Asteroid(Launcher app, double scale, BufferedImage image, int x, int y, int vx, int vy, int angle, int maxX, int maxY, int damage) {
+        super(app, scale, image, x, y, vx, vy, angle, maxX, maxY);
+        this.damage = damage;
+    }
+
+    public boolean onCollision(GameObject gameObject) {
+        switch (gameObject.getClass().getSimpleName()) {
+            case "Asteroid":
+            case "Moon":
+                return true;
+            case "Rocket":
+                // flying
+                if (((Rocket) gameObject).getIsFlying()) {
+                    return ((Rocket) gameObject).takeDamage(getDamage());
+                }
+                // landed
+                else {
+                    return true;
+                }
+        }
+        return false;
+    }
+
+    public void update() {
+        // go straight
+        this.vx = (int) Math.round(this.R * this.app.getScale() * this.scale * Math.cos(Math.toRadians(this.angle)));
+        this.vy = (int) Math.round(this.R * this.app.getScale() * this.scale * Math.sin(Math.toRadians(this.angle)));
+        this.x += this.vx;
+        this.y += this.vy;
+        // if the new position is out of current panel
+        if (!validPosition(this.x, this.y)) {
+            if (this.x < 0) {
+                this.x += this.maxX;
+            }
+            else if (this.x > this.maxX) {
+                this.x -= this.maxX;
+            }
+            if (this.y < 0) {
+                this.y += this.maxY;
+            }
+            else if (this.y > this.maxY) {
+                this.y -= this.maxY;
+            }
+        }
+    }
+
+    public int getDamage() {
+        return this.damage;
+    }
+}
---
+++ src/galacticmail/gameobject/Moon.java
@@ -0,0 +1,62 @@
+package galacticmail.gameobject;
+
+import java.awt.image.BufferedImage;
+import java.lang.Math;
+
+import galacticmail.Launcher;
+
+public class Moon extends GameMovableObject {
+    private final int R = 1;
+
+    private int pay;
+    private int penalty;
+
+    public Moon(Launcher app, double scale, BufferedImage image, int x, int y, int vx, int vy, int angle, int maxX, int maxY, int pay, int penalty) {
+        super(app, scale, image, x, y, vx, vy, angle, maxX, maxY);
+        this.pay = pay;
+        this.penalty = penalty;
+    }
+
+    public boolean onCollision(GameObject gameObject) {
+        switch (gameObject.getClass().getSimpleName()) {
+            case "Asteroid":
+                return false;
+            case "Moon":
+                return true;
+            case "Rocket":
+                return false;
+        }
+        return false;
+    }
+
+    public void update() {
+        // go straight
+        this.vx = (int) Math.round(this.R * this.app.getScale() * this.scale * Math.cos(Math.toRadians(this.angle)));
+        this.vy = (int) Math.round(this.R * this.app.getScale() * this.scale * Math.sin(Math.toRadians(this.angle)));
+        this.x += this.vx;
+        this.y += this.vy;
+        // if the new position is out of current panel
+        if (!validPosition(this.x, this.y)) {
+            if (this.x < 0) {
+                this.x += this.maxX;
+            }
+            else if (this.x > this.maxX) {
+                this.x -= this.maxX;
+            }
+            if (this.y < 0) {
+                this.y += this.maxY;
+            }
+            else if (this.y > this.maxY) {
+                this.y -= this.maxY;
+            }
+        }
+    }
+
+    public int getPay() {
+        return this.pay;
+    }
+
+    public int getPenalty() {
+        return this.penalty;
+    }
+}
